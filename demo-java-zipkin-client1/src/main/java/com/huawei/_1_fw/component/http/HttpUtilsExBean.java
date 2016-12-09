e14ab01a130cdaa56ea2e10762c0d8cca79de53e
JHerculesqz <JHercules_qz@qq.com>
2016-12-09 10:51:34 +0800

    fix "Header is too long"

--- demo-java-zipkin-client1/src/main/java/com/huawei/_1_fw/component/http/HttpUtilsExBean.java
+++ demo-java-zipkin-client1/src/main/java/com/huawei/_1_fw/component/http/HttpUtilsExBean.java
@@ -3,6 +3,7 @@ package com.huawei._1_fw.component.http;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
@@ -28,7 +29,19 @@ public class HttpUtilsExBean {
 	public <T> T postByTemplate(String strUrl, Object oPostParam, Class<T> oResponseClazz) {
 		MultiValueMap<String, String> headers = new LinkedMultiValueMap<String, String>();
 		headers.add("Content-Type", "application/json");
-		restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+
+		// πÿ“Ø“Øœ‘¡È
+		boolean bIsExists = false;
+		for (HttpMessageConverter<?> oConverter : restTemplate.getMessageConverters()) {
+			if (oConverter instanceof MappingJackson2HttpMessageConverter) {
+				bIsExists = true;
+				break;
+			}
+		}
+		if (!bIsExists) {
+			restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+		}
+
 		HttpEntity<Object> oRequest = new HttpEntity<Object>(oPostParam, headers);
 		return restTemplate.postForObject(strUrl, oRequest, oResponseClazz);
 	}
